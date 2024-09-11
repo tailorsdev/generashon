0932e1dd59f5cd04affd055a94ee1575b5ec045f
tailorsdev <166792141+tailorsdev@users.noreply.github.com>
2024-09-11 17:52:33 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-[#1e214b] h-full md:h-screen pb-20 relative">
+    <div class="absolute z-0 opacity-20 w-full h-full pt-32" >
+     <img src="/img/background.png" class="m-auto" alt="">
+    </div>
+    <div class="relative z-10" >
+      <img src="/img/logo.png" class="mx-auto pt-6 w-40" alt="" />
+      <div class="text-center pt-6 md:w-1/2 px-10 md:px-0 m-auto">
+        <h2 class="text-[#0cc0df] text-xl font-bold">
+          AKADEMIA GENERASHON NOBO
+        </h2>
+        <div class="text-white font-bold space-y-4 mt-4">
+          <p>
+            Plataforma diseñá pa personanan ku ta deseá na obtené mas
+            konosementu, krese i desaroya nan potenshal Akademia Generashon Nobo
+          </p>
+          <p>
+            Prepara bo mes pa bo transforma bo bida i uni na akademia generashon
+            nobo,un kaminda pa konosementu di desaroyo, pasado, presente i
+            futuro di paisnan di Reino Hulandes, un estudio di nan realidatnan
+            istoriko, sosial, ekonomiko, politiko i kultural den un espasio ku
+            ta promové e identidat pa nos teritorionan i kon nos por ta miho.
+          </p>
+          <p>
+            Huntu ku nos lo bo haña workshops, charlanan, masterclass,
+            seminarionan, konbersatorio i muchu mas. Uní ku nos.Nos ta warda bo.
+          </p>
+        </div>
+      </div>
+      <div
+        class="bg-black w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 rounded-lg"
+      ></div>
+     
+      <div class="fixed bottom-0 bg-[#0cc0df] w-full">
+        <h2 class="text-[#1e214b] text-center font-bold py-2 text-xl">
+          SABIDURÍA DEN DESAROYO
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
