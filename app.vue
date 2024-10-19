a29e1444ac9cec5053ce0d268b6d3c553c35d167
Jhon1505e <tailorsdevcom@gmail.com>
2024-10-18 20:01:20 -0500

    changes

--- app.vue
+++ app.vue
@@ -1,47 +1,92 @@
 <template>
-  <div class="bg-[#1e214b] h-full md:h-screen pb-20 relative">
+  <div class="bg-[#1e214b] h-full pb-20 relative">
     <div class="absolute z-0 opacity-20 w-full h-full pt-32">
       <img src="/img/background.png" class="m-auto" alt="" />
     </div>
-    <div class="relative z-10">
-      <div class="flex justify-between bg-[#1e214b]/80 py-4 backdrop-blur-sm px-6 md:px-10 fixed w-full">
-        <img src="/img/logoText.png" class=" w-1/2 md:w-52" alt="" />
+    <div class="relative z-10 pb-20 sm:pb-10">
+      <div
+        class="flex justify-between bg-[#1e214b]/80 py-4 backdrop-blur-sm px-6 md:px-10 fixed w-full"
+      >
+        <img src="/img/logoText.png" class="w-1/2 md:w-52" alt="" />
         <div>
           <button
             class="border-[#0cc0df] bg-[#0cc0df]/20 border text-[#0cc0df] rounded-xl px-4 py-2 mt-2.5 md:mt-4 font-bold flex gap-2"
-          > 
-            <span class="hidden md:block">Formulario</span> 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" class="size-6" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"/></svg>
+          >
+            <span class="hidden md:block">Formulario</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              class="size-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+              />
+            </svg>
           </button>
         </div>
       </div>
       <div class="text-center pt-32 md:w-1/2 px-10 md:px-0 m-auto">
-        <h2 class="text-[#0cc0df] text-xl font-bold">
+        <h2 class="text-[#0cc0df] text-3xl font-bold">
           AKADEMIA GENERASHON NOBO
         </h2>
-        <div class="text-white font-bold space-y-4 mt-4">
+        <div class="text-white font-bold space-y-4 mt-6">
+          <h2 class="text-[#0cc0df] text-xl font-bold">
+            INTELIGENSHA I SABIDURIA <br />
+            ¡DEN UN!
+          </h2>
           <p>
-            Plataforma diseñá pa personanan ku ta deseá na obtené mas
-            konosementu, krese i desaroya nan potenshal Akademia Generashon Nobo
+            Nos ta e plataforma akademiko ideal pa enseñansa, edukashon,
+            formashon, kapasitashon, profeshonalismo, kursonan di
+            formashon,seminario, simposio i debate,masterclass i entrevistanan,
+            den e búskeda permanente di konosementu den tur e areanan di e
+            prosesonan humano tantu filosofiko komo sientifiko, ku e obhetivo pa
+            generashon di un pensamentu global akorde ku e eksigenshanan di nos
+            sosiedatnan den nan demandanan konstante pa kambio.
           </p>
+          <div
+            class="text-[#0cc0df] m-auto flex justify-center text-xl px-10 md:px-0"
+          >
+            <ul class="list-decimal text-start">
+              <li>
+                No perde e oportunidat pa huntu nos siña for di otro i komparti
+                eksperensha!
+              </li>
+              <li>Jion ku nos pa huntu nos avansá</li>
+            </ul>
+          </div>
           <p>
-            Prepara bo mes pa bo transforma bo bida i uni na akademia generashon
-            nobo,un kaminda pa konosementu di desaroyo, pasado, presente i
-            futuro di paisnan di Reino Hulandes, un estudio di nan realidatnan
-            istoriko, sosial, ekonomiko, politiko i kultural den un espasio ku
-            ta promové e identidat pa nos teritorionan i kon nos por ta miho.
-          </p>
-          <p>
-            Huntu ku nos lo bo haña workshops, charlanan, masterclass,
-            seminarionan, konbersatorio i muchu mas. Uní ku nos.Nos ta warda bo.
+            Abona pa bo ta on ku nos tur djasabra 11.am ora Korsou i 5pm ora
+            Hulanda i haña bo masterclassnan,charla,konferensha,simposio ku e
+            miho konferensistanan.Kursonan di kapasitashon i formashon pa bo ta
+            up to date ku desaroyonan lokal,den reino i mundu. Una bes bo
+            abona,nos ta manda e link pabo pa bo tin akseso na nos aktividatnan
+            di AGN. Primi riba e botón INSKRIPSHON i yena e formulario i realisa
+            bo pago pa e balor di fl.75 pa luna.Bo suskripshon mester ta mínimo
+            pa tres luna.Por realisa esaki pa medio di un standing order.Una bes
+            bo pago keda realisa,manda pa nos e komprobante di pago na nos mail
+            akademiagn2025@gmail.com i mesora nos lo tuma kontakto kubo.
           </p>
         </div>
       </div>
-      <div class="bg-black w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-20 rounded-lg">
-        
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/I-8djD5dJmM?si=oEB5-PBjEtTzkInP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <div
+        class=" w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-20  rounded-lg"
+      >
+        <iframe
+          width="100%"
+          height="315"
+          src="https://www.youtube.com/embed/I-8djD5dJmM?si=oEB5-PBjEtTzkInP"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
       </div>
-      <p>-</p>
       <div class="fixed bottom-0 bg-[#ffde59] w-full">
         <h2 class="text-[#1e214b] text-center font-bold py-2 text-xl">
           SABIDURÍA DEN DESAROYO
