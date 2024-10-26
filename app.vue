6f76623216dc71ef8db258c73f6696a8c3ae8d17
Jhon1505e <tailorsdevcom@gmail.com>
2024-10-26 17:08:30 -0500

    nuevo texto y formulario

--- app.vue
+++ app.vue
@@ -4,38 +4,12 @@
       <img src="/img/background.png" class="m-auto" alt="" />
     </div>
     <div class="relative z-10 pb-20 sm:pb-10">
-      <div
-        class="flex justify-between bg-[#1e214b]/80 py-4 backdrop-blur-sm px-6 md:px-10 fixed w-full"
-      >
-        <img src="/img/logoText.png" class="w-1/2 md:w-52" alt="" />
-        <div>
-          <button
-            class="border-[#0cc0df] bg-[#0cc0df]/20 border text-[#0cc0df] rounded-xl px-4 py-2 mt-2.5 md:mt-4 font-bold flex gap-2"
-          >
-            <span class="hidden md:block">Formulario</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="1.5"
-              class="size-6"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-      <div class="text-center pt-32 md:w-1/2 px-10 md:px-0 m-auto">
-        <h2 class="text-[#0cc0df] text-3xl font-bold">
-          AKADEMIA GENERASHON NOBO
-        </h2>
-        <div class="text-white font-bold space-y-4 mt-6">
-          <h2 class="text-[#0cc0df] text-xl font-bold">
+  
+      <div class="text-center pt-20 md:w-1/2 px-10 md:px-0 m-auto">
+        <img src="/img/logoText.png" class="w-60 md:w-72 mx-auto" alt="" />
+        
+        <div class="text-white font-bold space-y-4 mt-10">
+          <h2 class="text-[#0cc0df] text-3xl font-bold">
             INTELIGENSHA I SABIDURIA <br />
             ¡DEN UN!
           </h2>
@@ -49,9 +23,9 @@
             sosiedatnan den nan demandanan konstante pa kambio.
           </p>
           <div
-            class="text-[#0cc0df] m-auto flex justify-center text-xl px-10 md:px-0"
+            class="text-[#0cc0df] m-auto flex justify-center md:w-2/3 text-xl px-6 md:px-0"
           >
-            <ul class="list-decimal text-start">
+            <ul class="list-decimal text-start space-y-5">
               <li>
                 No perde e oportunidat pa huntu nos siña for di otro i komparti
                 eksperensha!
@@ -72,9 +46,13 @@
             akademiagn2025@gmail.com i mesora nos lo tuma kontakto kubo.
           </p>
         </div>
+
+        <div class="mt-10">
+<a target="_blank" href="https://forms.gle/sycd6C2N8wbVqbAf9" class="px-4  bg-[#0cc0df] rounded-lg py-3 text-xl text-white font-bold">djòin e komunidat </a>
+        </div>
       </div>
       <div
-        class=" w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-20  rounded-lg"
+        class=" w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-14  rounded-lg"
       >
         <iframe
           width="100%"
@@ -87,7 +65,7 @@
           allowfullscreen
         ></iframe>
       </div>
-      <div class="fixed bottom-0 bg-[#ffde59] w-full">
+      <div class="fixed top-0 bg-[#ffde59] w-full">
         <h2 class="text-[#1e214b] text-center font-bold py-2 text-xl">
           SABIDURÍA DEN DESAROYO
         </h2>
