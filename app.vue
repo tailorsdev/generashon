d47aef0d57c8ef685c7b8f29b69bd6452653f550
Jhon1505e <tailorsdevcom@gmail.com>
2024-10-26 17:41:17 -0500

    cambios

--- app.vue
+++ app.vue
@@ -4,10 +4,9 @@
       <img src="/img/background.png" class="m-auto" alt="" />
     </div>
     <div class="relative z-10 pb-20 sm:pb-10">
-  
       <div class="text-center pt-20 md:w-1/2 px-10 md:px-0 m-auto">
         <img src="/img/logoText.png" class="w-60 md:w-72 mx-auto" alt="" />
-        
+
         <div class="text-white font-bold space-y-4 mt-10">
           <h2 class="text-[#0cc0df] text-3xl font-bold">
             INTELIGENSHA I SABIDURIA <br />
@@ -25,7 +24,7 @@
           <div
             class="text-[#0cc0df] m-auto flex justify-center md:w-2/3 text-xl px-6 md:px-0"
           >
-            <ul class="list-decimal text-start space-y-5">
+            <ul class="list-disc text-start space-y-5">
               <li>
                 No perde e oportunidat pa huntu nos siña for di otro i komparti
                 eksperensha!
@@ -34,26 +33,33 @@
             </ul>
           </div>
           <p>
-            Abona pa bo ta on ku nos tur djasabra 11.am ora Korsou i 5pm ora
-            Hulanda i haña bo masterclassnan,charla,konferensha,simposio ku e
-            miho konferensistanan.Kursonan di kapasitashon i formashon pa bo ta
-            up to date ku desaroyonan lokal,den reino i mundu. Una bes bo
-            abona,nos ta manda e link pabo pa bo tin akseso na nos aktividatnan
-            di AGN. Primi riba e botón INSKRIPSHON i yena e formulario i realisa
-            bo pago pa e balor di fl.75 pa luna.Bo suskripshon mester ta mínimo
-            pa tres luna.Por realisa esaki pa medio di un standing order.Una bes
-            bo pago keda realisa,manda pa nos e komprobante di pago na nos mail
-            akademiagn2025@gmail.com i mesora nos lo tuma kontakto kubo.
+            Abóna pa bo ta ón ku nos tur djásabra 11 a.m. ora Kòrsou i 5 p.m.
+            ora Hulándia i haña bo masterclásnan, charla, konferensha, simposio
+            ku e miho konferensistannan. Kursonan di kapasitashón i formashon pa
+            bo ta up to date ku desaroyonan lokal, den reino i mundu. <br />
+            Una bès bo abóna, nos ta manda e link pa bo pa bo tin akseso na nos
+            aktividatnan di AGN. Primi riba e botón INSKRIPSHON i yena e
+            formulario i realisa bo pago pa e valor di fl. 75 pa luna. Bo
+            suskripshon mester ta mínimo pa tres luna. Por realisa esaki pa
+            medio di un standing order. <br />
+
+            Una bès bo pago keda realisa, manda pa nos e komprobante di pago na
+            nos mail
+            <span class="text-[#0cc0df]"> akademiagn2025@gmail.com</span> i
+            mesora nos lo tuma kontakto kubo.
           </p>
         </div>
 
         <div class="mt-10">
-<a target="_blank" href="https://forms.gle/sycd6C2N8wbVqbAf9" class="px-4  bg-[#0cc0df] rounded-lg py-3 text-xl text-white font-bold">djòin e komunidat </a>
+          <a
+            target="_blank"
+            href="https://forms.gle/sycd6C2N8wbVqbAf9"
+            class="px-4 bg-[#0cc0df] rounded-lg py-3 text-lg text-white font-bold"
+            >Inscribí bò ku
+          </a>
         </div>
       </div>
-      <div
-        class=" w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-14  rounded-lg"
-      >
+      <div class="w-5/6 h-60 md:w-1/2 m-auto md:h-80 mt-10 md:mt-14 rounded-lg">
         <iframe
           width="100%"
           height="315"
